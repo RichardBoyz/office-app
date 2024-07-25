a8ddd1748df9838737a7df6d46843e5ef61fe426
RichardBoyz <r1382333@gmail.com>
2024-07-26 00:34:00 +0800

    Add shadcn

--- src/components/chores/ChoresNavigator.tsx
+++ src/components/chores/ChoresNavigator.tsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
 const ChoresNavigator = () => {
-  const [currentTab, setCurrentTab] = useState<string>("search");
-
-  const handleOnValueChange = (value: string) => {
-    setCurrentTab(value);
-  };
   return (
     <div className="flex-auto flex flex-col items-center">
       {/* <CreateChoresRoom /> */}
-      <div>123</div>
     </div>
   );
 };
