ce3a4fafffe9d247a836f0fa7577f5a2d722cbeb
RichardBoyz <r1382333@gmail.com>
2024-08-17 20:13:44 +0800

    Adjust chores card style

--- src/components/chores/ChoresRoomCard.tsx
+++ src/components/chores/ChoresRoomCard.tsx
@@ -14,7 +14,8 @@ const ChoresRoomCard = ({ roomId, name, creator }: ChoresRoomCardType) => {
   return (
     <div
       onClick={handleClick}
-      className="hover:scale-105 transition-transform h-[400px] flex rounded-md shadow-md border border-slate-400 px-2 pt-6 pb-2 flex-col"
+      className="hover:scale-105 transition-transform h-[400px] flex rounded-md shadow-md border border-slate-400 px-2 pt-6 pb-2 flex-col cursor-pointer"
+      title={`要不要進入 ${name} 呀`}
     >
       <div className="flex gap-1">
         <span className="shrink-0">名稱：</span>
